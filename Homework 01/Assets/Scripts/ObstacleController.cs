8bd963043d25ccd751e2770a4fee67934ea246b2
Starkey <starkey8203@gmail.com>
2024-09-09 22:01:38 -0500

    Added "UFO Sample" and modified some items. Facing a bug with reflections. Could use material, but then player bounces. Still thinking

--- Homework 01/Assets/Scripts/ObstacleController.cs
+++ Homework 01/Assets/Scripts/ObstacleController.cs
@@ -1,18 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ObstacleController : MonoBehaviour
 {
+    public float speed = 5;
+    private Angle sinFunction;
+    private float angleVel = 45f;
+    private Vector2 velocityDir;
+    private Rigidbody2D obstacleBody;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        float randomX = Random.Range(-1.0f, 1.0f);
+        float randomY = Random.Range(-1.0f, 1.0f);
+        velocityDir = new Vector2(randomX*speed, randomY*speed);
+        obstacleBody = GetComponent<Rigidbody2D>();
+        obstacleBody.velocity = velocityDir;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, angleVel * Time.deltaTime);
     }
+
+    /*
+    public void OnCollisionEnter2D(Collision2D otherObject)
+    {
+        if(otherObject.gameObject.CompareTag("Background"))
+        {
+            bool movingRight = obstacleBody.velocity.x >= 0;
+            bool movingUp = obstacleBody.velocity.y >= 0;
+            bool reflected = false;
+
+            if(movingRight)
+            {
+                if ((otherObject.transform.position.y >= transform.position.y) && movingUp)
+                {
+                    velocityDir.y *= -1;
+                    obstacleBody.velocity = velocityDir;
+                    reflected = true;
+                }
+                if ((otherObject.transform.position.x >= transform.position.x) && !reflected)
+                {
+                    velocityDir.x *= -1;
+                    obstacleBody.velocity = velocityDir;
+                }
+            } else
+            {
+
+            }
+
+            //is otherObject above us?
+            
+
+            //is otherObject below us?
+            if ((otherObject.transform.position.y <= transform.position.y) && !reflected)
+            {
+                velocityDir.y *= -1;
+                obstacleBody.velocity = velocityDir;
+                reflected = true;
+            }
+
+            //is OtherObject left of us?
+            if ((otherObject.transform.position.x >= transform.position.x) && !reflected)
+            {
+                velocityDir.x *= -1;
+                obstacleBody.velocity = velocityDir;
+                reflected = true;
+            }
+
+            //is OtherObject right of us?
+            
+        }
+    }
+    */
+
 }
--- Homework 01/Assets/Scripts/PlayerController.cs
+++ Homework 01/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public Text timerText;
+    public Text victoryText;
+    public Button restartButton;
+
+    private Rigidbody2D playerBody;
+    private float timer = 0.0f;
+    private int gameDuration = 60; //60 seconds long
+    private float captureTime;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerBody = GetComponent<Rigidbody2D>();
+        timer = 0;
+        timerText.text = "Time to go: " + timer.ToString();
+        victoryText.text = "";
+        restartButton.gameObject.SetActive(false);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        timer += Time.deltaTime;
+        int seconds = (int)timer % 60;
+        int timeToGo = gameDuration - seconds;
+        timerText.text = "Time to go: " + timeToGo.ToString();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        playerBody.velocity = moveDirection * speed;
+        CheckTimer();
+    }
+
+    public void OnCollisionEnter2D(Collision2D otherObject)
+    {
+        if(otherObject.gameObject.CompareTag("Obstacle"))
+        {
+            captureTime = timer*100;
+            captureTime = (int)captureTime;
+            captureTime = captureTime / 100;
+            timerText.gameObject.SetActive(false);
+            victoryText.text = "You Survived for " + captureTime.ToString() + " seconds";
+            restartButton.gameObject.SetActive(true);
+        }
+
+        if (otherObject.gameObject.CompareTag("Background"))
+        {
+            playerBody.velocity = Vector2.zero;
+        }
+    }
+
+    private void CheckTimer()
+    {
+        if (timer > gameDuration)
+        {
+            captureTime = gameDuration;
+            timerText.gameObject.SetActive(false);
+            victoryText.text = "You Win!\\ You Survived for " + captureTime.ToString() + " seconds";
+            restartButton.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnRestartButtonPress()
+    {
+        SceneManager.LoadScene("MainScene");
     }
 }
