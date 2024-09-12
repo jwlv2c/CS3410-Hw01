4dad39702fa461a84267fd649b518abcc60a7089
Starkey <starkey8203@gmail.com>
2024-09-11 19:30:59 -0500

    Fixed bouncing bug

--- Homework 01/Assets/Scripts/ObstacleController.cs
+++ Homework 01/Assets/Scripts/ObstacleController.cs
@@ -9,22 +9,34 @@ public class ObstacleController : MonoBehaviour
     private Angle sinFunction;
     private float angleVel = 45f;
     private Vector2 velocityDir;
-    private Rigidbody2D obstacleBody;
-    
+    private Rigidbody2D selfBody;
+    private Vector3 lastVelocity;
+
+    public float bounceFactor = 1; //0 - no bounce, 1 - reflect bounce, >1 - overbounce
+
     // Start is called before the first frame update
     void Start()
     {
         float randomX = Random.Range(-1.0f, 1.0f);
         float randomY = Random.Range(-1.0f, 1.0f);
         velocityDir = new Vector2(randomX*speed, randomY*speed);
-        obstacleBody = GetComponent<Rigidbody2D>();
-        obstacleBody.velocity = velocityDir;
+        selfBody = GetComponent<Rigidbody2D>();
+        selfBody.velocity = velocityDir;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, 0, angleVel * Time.deltaTime);
+        lastVelocity = selfBody.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D otherObject) 
+    {
+        var speed = lastVelocity.magnitude;
+        var direction = Vector3.Reflect(lastVelocity.normalized, otherObject.contacts[0].normal);
+
+        selfBody.velocity = direction * speed * bounceFactor;
     }
 
     /*
@@ -32,8 +44,8 @@ public class ObstacleController : MonoBehaviour
     {
         if(otherObject.gameObject.CompareTag("Background"))
         {
-            bool movingRight = obstacleBody.velocity.x >= 0;
-            bool movingUp = obstacleBody.velocity.y >= 0;
+            bool movingRight = selfBody.velocity.x >= 0;
+            bool movingUp = selfBody.velocity.y >= 0;
             bool reflected = false;
 
             if(movingRight)
@@ -41,13 +53,13 @@ public class ObstacleController : MonoBehaviour
                 if ((otherObject.transform.position.y >= transform.position.y) && movingUp)
                 {
                     velocityDir.y *= -1;
-                    obstacleBody.velocity = velocityDir;
+                    selfBody.velocity = velocityDir;
                     reflected = true;
                 }
                 if ((otherObject.transform.position.x >= transform.position.x) && !reflected)
                 {
                     velocityDir.x *= -1;
-                    obstacleBody.velocity = velocityDir;
+                    selfBody.velocity = velocityDir;
                 }
             } else
             {
@@ -61,7 +73,7 @@ public class ObstacleController : MonoBehaviour
             if ((otherObject.transform.position.y <= transform.position.y) && !reflected)
             {
                 velocityDir.y *= -1;
-                obstacleBody.velocity = velocityDir;
+                selfBody.velocity = velocityDir;
                 reflected = true;
             }
 
@@ -69,7 +81,7 @@ public class ObstacleController : MonoBehaviour
             if ((otherObject.transform.position.x >= transform.position.x) && !reflected)
             {
                 velocityDir.x *= -1;
-                obstacleBody.velocity = velocityDir;
+                selfBody.velocity = velocityDir;
                 reflected = true;
             }
 
